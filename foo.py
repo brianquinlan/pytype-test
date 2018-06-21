ac53dbd65f80f0b9111a826ae258579ca09155ab
Brian Quinlan <brian@sweetapp.com>
2018-06-21 10:08:13 -0700

    More type errors

--- foo.py
+++ foo.py
@@ -1,5 +1,9 @@
 def make_greeting(user_id):
     return 'hello, user' + user_id
 
-def print_greeting():
-    print(make_greeting('0'))
+def print_greeting(i):
+    print(make_greeting(i))
+    
+print_greeting(0)
+print_greeting(3.0)
+print_greenting([1,2,3])
