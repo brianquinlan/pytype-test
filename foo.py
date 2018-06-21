8de504e1abda11fcd346fcbcf186e7d843e8a0b1
Brian Quinlan <brian@sweetapp.com>
2018-06-21 11:01:42 -0700

    Defailt sy

--- foo.py
+++ foo.py
@@ -2,5 +2,5 @@ def make_greeting(user_id):
     return 'hello, user' + user_id
 
 def print_greeting():
-    print(make_greeting('0'))
-    print(make_greeting('1.1'))
+    print(make_greeting(0))
+    print(make_greeting(1.1))
