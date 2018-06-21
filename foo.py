6ac23e27fbac84bca166a398dc1d29267895c946
Brian Quinlan <brian@sweetapp.com>
2018-06-21 10:27:23 -0700

    Add quotes

--- foo.py
+++ foo.py
@@ -4,6 +4,6 @@ def make_greeting(user_id):
 def print_greeting(i):
     print(make_greeting(i))
     
-print_greeting(0)
-print_greeting(3.0)
-print_greenting([1,2,3])
+print_greeting('0')
+print_greeting('3.0')
+print_greenting('[1,2,3]')
