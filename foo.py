ebd42fa6162f25957877ba03e565085fe80f504b
Brian Quinlan <brian@sweetapp.com>
2018-06-22 13:28:18 -0700

    Broken

--- foo.py
+++ foo.py
@@ -4,6 +4,6 @@ def make_greeting(user_id):
     return 'hello, user' + user_id
 
 def print_greeting():
-    print(make_greeting('123'))
+    print(make_greeting(123))
 
-print(itertools.chain([1,2,3]))
+print(itertools.chain(1,2,3))
