6655e077eebc43487c945f5ce7554c76c371f577
Brian Quinlan <brian@sweetapp.com>
2018-06-21 19:31:24 -0700

    Chain

--- foo.py
+++ foo.py
@@ -1,5 +1,9 @@
+import itertools
+
 def make_greeting(user_id):
     return 'hello, user' + user_id
 
 def print_greeting():
-    print(make_greeting('123'))
+    print(make_greeting(123))
+
+print(itertools.chain(1,2,3))
