94bc30fad1113bf2a007eb48f19f0ebff71921a2
Brian Quinlan <brian@sweetapp.com>
2018-06-22 17:10:58 -0700

    Very broken

--- foo.py
+++ foo.py
@@ -4,13 +4,13 @@ def make_greeting(user_id):
     return 'hello, user' + user_id
 
 def print_greeting():
-    print(make_greeting('123'))
+    print(make_greeting(123))
 
-print(itertools.chain([1,2,3]))
+print(itertools.chain(1,2,3))
 
 bar()
 
-# from catfood import *
+from catfood import *
 
 foo = [1,2,3]
 baz = foo + 3
