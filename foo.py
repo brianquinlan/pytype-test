b0eaf3640f860d5f2ea709579bf0897657dbe75e
Brian Quinlan <brian@sweetapp.com>
2018-06-25 10:17:03 -0700

    Comment stuff out

--- foo.py
+++ foo.py
@@ -6,6 +6,7 @@ def make_greeting(user_id):
 def print_greeting():
     print(make_greeting(123))
 
+'''
 print(itertools.chain(1,2,3))
 
 bar()
@@ -14,3 +15,4 @@ from catfood import *
 
 foo = [1,2,3]
 baz = foo + 3
+'''
