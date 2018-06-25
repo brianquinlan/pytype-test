7c35d8ad89ef87202dd62c1be9302848042c413e
Brian Quinlan <brian@sweetapp.com>
2018-06-25 10:17:37 -0700

    Base example

--- foo.py
+++ foo.py
@@ -5,14 +5,3 @@ def make_greeting(user_id):
 
 def print_greeting():
     print(make_greeting(123))
-
-'''
-print(itertools.chain(1,2,3))
-
-bar()
-
-from catfood import *
-
-foo = [1,2,3]
-baz = foo + 3
-'''
